8fe2737108538a32a27003880e3ad98ee92c34a6
GoncaloGarcia <GGFPC12495@gmail.com>
2019-07-17 12:39:35 +0100

    Remove thread ID

--- cassandra/src/main/java/AppAspect.java
+++ cassandra/src/main/java/AppAspect.java
@@ -12,7 +12,7 @@ public class AppAspect {
 
     @After("execution(boolean com.datastax.driver.core.RequestHandler.SpeculativeExecution.query(..))")
     public void runAfterCassandra(JoinPoint point){
-        String IP = point.getArgs()[0].toString() + "  " + Thread.currentThread().getId();
+        String IP = point.getArgs()[0].toString();
         if(GlobalTracer.isRegistered() && GlobalTracer.get().activeSpan() != null){
             System.out.println(GlobalTracer.get().activeSpan());
             GlobalTracer.get().activeSpan().setTag("Cassandra-IP", IP);
